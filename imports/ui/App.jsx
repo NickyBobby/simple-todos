9dfca764c20fadc8455c3962d0f936a028fff586
Nicholas Dorans <Nicky_Bobby@icloud.com>
2016-07-09 15:50:38 -0600

    Adds Collection functionality

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,23 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+
+import { Tasks } from '../api/tasks.js';
 
 import Task from './Task.jsx';
 
 // App component - represents the whole app
-export default class App extends Component {
-  getTasks() {
-    return [
-      { _id: 1, text: 'this is task 1' },
-      { _id: 2, text: 'this is task 2' },
-      { _id: 3, text: 'this is task 3' },
-    ];
-  }
-
+class App extends Component {
   renderTasks() {
-    return this.getTasks().map((task) => (
-      <Task key={task._id} task={task} />
+    return this.props.tasks.map((task) => (
+      <Task key={task.id} task={task} />
     ));
   }
-
+  
   render() {
     return (
       <div className="container">
@@ -32,3 +27,13 @@ export default class App extends Component {
     );
   }
 }
+
+App.propTypes = {
+  tasks: PropTypes.array.isRequired,
+};
+
+export default createContainer(() => {
+  return {
+    tasks: Tasks.find({}).fetch(),
+  };
+}, App);
